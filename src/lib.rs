37ba7700541bf87115505bb42e90c7012f781b50
Moritz Moeller <virtualritz@gmail.com>
2020-10-29 12:58:25 +0100

    Minimized num deps.

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,17 @@
 use core::marker::PhantomData;
 use core::ops::Add;
 use core::ops::Mul;
-use num::{
+/*use num::{
     cast::AsPrimitive,
-    traits::identities::{One, Zero}, // , ops::mul_add::MulAdd},
     Float,
     FromPrimitive,
+};*/
+use num_traits::{
+    cast::{AsPrimitive, FromPrimitive},
+    float::Float,
+    identities::{Zero, One}
 };
 use roots::{find_root_brent, FloatType, SimpleConvergency};
-
 
 pub trait Basis<T: Float> {
     const STEP: usize;
@@ -27,6 +30,18 @@ impl Basis<f32> for CatmullRom {
         [0.0 / 2.0, 2.0 / 2.0, 0.0 / 2.0, 0.0 / 2.0],
     ];
 }
+
+
+impl Basis<f64> for CatmullRom {
+    const STEP: usize = 1;
+    const MATRIX: [[f64; 4]; 4] = [
+        [-1.0 / 2.0, 3.0 / 2.0, -3.0 / 2.0, 1.0 / 2.0],
+        [2.0 / 2.0, -5.0 / 2.0, 4.0 / 2.0, -1.0 / 2.0],
+        [-1.0 / 2.0, 0.0 / 2.0, 1.0 / 2.0, 0.0 / 2.0],
+        [0.0 / 2.0, 2.0 / 2.0, 0.0 / 2.0, 0.0 / 2.0],
+    ];
+}
+
 
 pub struct Bezier;
 
@@ -49,12 +64,12 @@ where
     T: AsPrimitive<usize> + FloatType + Float + FromPrimitive + Ord + One + Zero,
     B: Basis<T>,
 {
-    pub fn evaluate<U>(x: T, knots: Vec<U>) -> U
+    pub fn evaluate<U>(x: T, knots: &Vec<U>) -> U
     where
         U: Add<Output = U> + Copy + Mul<T, Output = U> + Zero,
     {
         let number_of_segments: usize = ((knots.len() - 4) / B::STEP) + 1;
-        let mut x: T = x.clamp(num::Zero::zero(), num::One::one()) * T::from_usize(number_of_segments).unwrap();
+        let mut x: T = x.clamp(num_traits::Zero::zero(), num_traits::One::one()) * T::from_usize(number_of_segments).unwrap();
 
         //let seg_x: T = x;
         //let segment = (seg_x.as_()).clamp(0, number_of_segments - 1);
@@ -81,7 +96,7 @@ where
 
     /// Evaluate the inverse of a spline; i.e., solve for
     /// the x for which `evaluate(x)` == y.
-    pub fn evaluate_inverse<U>(y: T, knots: Vec<T>) -> Option<T> {
+    pub fn evaluate_inverse<U>(y: T, knots: &Vec<T>) -> Option<T> {
         // Account for out-of-range inputs;
         // just clamp to the values we have.
         let low_index: usize = if B::STEP == 1 { 1 } else { 0 };
@@ -95,28 +110,28 @@ where
         // If increasing ...
         if knots[1] < knots[knots.len() - 2] {
             if y <= knots[low_index] {
-                return Some(num::Zero::zero());
+                return Some(num_traits::Zero::zero());
             }
             if y >= knots[high_index] {
-                return Some(num::One::one());
+                return Some(num_traits::One::one());
             }
         } else {
             if y >= knots[low_index] {
-                return Some(num::Zero::zero());
+                return Some(num_traits::Zero::zero());
             }
             if y <= knots[high_index] {
-                return Some(num::One::one());
+                return Some(num_traits::One::one());
             }
         }
 
-        let spline_function = |x| Self::evaluate(x, knots.clone());
+        let spline_function = |x| Self::evaluate(x, knots);
 
         let mut convergency = SimpleConvergency {
             eps: T::from_f64(1e-6).unwrap(),
             max_iter: 32,
         };
 
-        match find_root_brent(y, num::Zero::zero(), &spline_function, &mut convergency) {
+        match find_root_brent(y, num_traits::Zero::zero(), &spline_function, &mut convergency) {
             Ok(x) => {
                 Some(x)
             }
@@ -124,3 +139,28 @@ where
         }
     }
 }
+
+/*
+use ultraviolet as uv;
+
+type Vec2 = uv::Vec2;
+
+impl num::Zero for Vec2 {
+    fn zero() -> Self {
+        Vec2::new(0., 0.)
+    }
+}*/
+
+#[test]
+
+    fn test () {
+        let spline = vec![
+            0.0,
+            0.4,
+            0.5,
+            0.9,
+            1.0,
+        ];
+
+        println!("{:?}", Interpolation::<_, CatmullRom>::evaluate(0.3f64, &spline));
+    }
