984209f048c6c1da7cf1ec4673d48b456e94879d
Moritz Moeller <virtualritz@gmail.com>
2020-11-03 23:14:12 +0100

    Fixed example.

--- README.md
+++ README.md
@@ -20,17 +20,17 @@ Using a combination of `spline()` and `spline_inverse()` it is
 possible to compute a full spline-with-nonuniform-abscissæ:
 ```rust
 use uniform_cubic_splines::{
-    spline, spline_inverse, basis::{CatmullRom, Linear}
+    spline, spline_inverse, basis::CatmullRom
 };
 
 // We want to evaluate the spline at knot value 0.3.
 let x = 0.3;
 
 // The first and last points are never interpolated.
+let knots =  [0.0, 0.0, 0.1, 0.3, 1.0, 1.0];
 let values = [0.0, 0.0, 1.3, 4.2, 3.2, 3.2];
-let knots = [0.0, 0.0, 0.1, 0.3, 1.0, 1.0];
 
-let v = spline_inverse::<Linear, _>(x, &knots).unwrap();
+let v = spline_inverse::<CatmullRom, _>(x, &knots).unwrap();
 let y = spline::<CatmullRom, _, _>(v, &values);
 
 assert!(y - 4.2 < 1e-6);
--- src/lib.rs
+++ src/lib.rs
@@ -22,17 +22,17 @@
 //! possible to compute a full spline-with-nonuniform-abscissæ:
 //! ```
 //! use uniform_cubic_splines::{
-//!     spline, spline_inverse, basis::{CatmullRom, Linear}
+//!     spline, spline_inverse, basis::CatmullRom
 //! };
 //!
 //! // We want to evaluate the spline at knot value 0.3.
 //! let x = 0.3;
 //!
 //! // The first and last points are never interpolated.
-//! let values = [0.0, 0.0, 1.3, 4.2, 3.2, 3.2];
 //! let knots =  [0.0, 0.0, 0.1, 0.3, 1.0, 1.0];
+//! let values = [0.0, 0.0, 1.3, 4.2, 3.2, 3.2];
 //!
-//! let v = spline_inverse::<Linear, _>(x, &knots).unwrap();
+//! let v = spline_inverse::<CatmullRom, _>(x, &knots).unwrap();
 //! let y = spline::<CatmullRom, _, _>(v, &values);
 //!
 //! assert!(y - 4.2 < 1e-6);
